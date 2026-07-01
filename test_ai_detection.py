2b99d17206d1498281980ebf9c15850231256934
xdewx <present150608@sina.com>
2026-07-01 19:08:35 +0800

    test: add merge_sorted function

--- test_ai_detection.py
+++ test_ai_detection.py
@@ -18,3 +18,17 @@ def binary_search(arr, target):
         else:
             right = mid - 1
     return -1
+
+def merge_sorted(arr1, arr2):
+    result = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i += 1
+        else:
+            result.append(arr2[j])
+            j += 1
+    result.extend(arr1[i:])
+    result.extend(arr2[j:])
+    return result
